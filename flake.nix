4761fe19241815a4208c1d75a8526e0ea6480fbb
MoeLeak <i@leak.moe>
2026-01-09 18:06:03 +0800

    feat: Initial project

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # (1) add mkdocs-flake input
+    mkdocs-flake.url = "github:applicative-systems/mkdocs-flake";
+  };
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    # (2) import mkdocs-flake module
+    imports = [
+      inputs.mkdocs-flake.flakeModules.default
+    ];
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+    perSystem = { config, self', inputs', pkgs, system, ... }: {
+      packages.default = pkgs.hello;
+
+      # (3) point mkdocs-flake to your mkdocs root folder
+      documentation.mkdocs-root = ./.;
+
+      # (4) Build the docs:
+      #     `nix build .#documentation`
+      #     Run in watch mode for live-editing-rebuilding:
+      #     `nix run .#watch-documentation`
+    };
+  };
+}
